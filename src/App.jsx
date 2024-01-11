985cdc3deca6ac1a1f206ba3088c83a7d3dba6be
biboyboy04 <55369988+biboyboy04@users.noreply.github.com>
2024-01-12 03:14:52 +0800

    Add Nav.jsx
    - Install react-icons
    - Add Side Nav
    - Use Nav on App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import ParticlesContainer from "./components/ParticlesContainer";
 import Home from "./components/Home";
+import Nav from "./components/Nav";
 export default function App() {
   return (
     <div className=" w-full xl:w-[1600px]">
       <ParticlesContainer />
+      <Nav />
       <Home />
     </div>
   );
