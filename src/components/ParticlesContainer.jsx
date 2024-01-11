082afe3d7fb7a87c5d84f1ea4c894bfe3e5861f7
biboyboy04 <55369988+biboyboy04@users.noreply.github.com>
2024-01-11 19:08:10 +0800

    Initital Setup
    - Add react with vite
    - Add tailwind

---
+++ src/components/ParticlesContainer.jsx
@@ -0,0 +1,109 @@
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadFull } from "tsparticles";
+import React, { useState, useEffect, useMemo, useCallback } from "react";
+const ParticlesContainer = () => {
+  const [init, setInit] = useState(false);
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+      // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+      // starting from v2 you can add only the features you need reducing the bundle size
+      //await loadAll(engine);
+      await loadFull(engine);
+
+      //await loadBasic(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+  const particlesLoaded = useCallback(async (container) => {
+    console.log(container);
+  }, []);
+  const options = useMemo(
+    () => ({
+      fullScreen: { enable: false },
+      background: {
+        color: {
+          value: "",
+        },
+      },
+      fpsLimit: 120,
+      interactivity: {
+        events: {
+          onclick: {
+            enable: true,
+            mode: "push",
+          },
+          onHover: {
+            enable: true,
+            mode: "repulse",
+          },
+          resize: true,
+        },
+        modes: {
+          push: {
+            quantity: 90,
+          },
+          repulse: {
+            distance: 200,
+            duration: 0.4,
+          },
+        },
+      },
+      particles: {
+        color: {
+          value: "#e68e2e",
+        },
+        links: {
+          color: "#f5d393",
+          distance: 150,
+          enable: true,
+          opacity: 0.5,
+          width: 1,
+        },
+        collisions: {
+          enable: true,
+        },
+        move: {
+          direction: "none",
+          enable: true,
+          outMode: {
+            default: "bounce",
+          },
+          random: false,
+          speed: 1,
+          straight: false,
+        },
+        number: {
+          density: {
+            enable: true,
+            area: 800,
+          },
+          value: 150,
+        },
+        opacity: {
+          value: 0.5,
+        },
+        shape: {
+          type: "circle",
+        },
+        size: {
+          value: { min: 1, max: 5 },
+        },
+      },
+      detectRetina: true,
+    }),
+    []
+  );
+
+  return (
+    <Particles
+      className="w-full h-full absolute top-0 left-0"
+      id="tsparticles"
+      init={init}
+      particlesLoaded={particlesLoaded}
+      options={options}
+    />
+  );
+};
+export default ParticlesContainer;
