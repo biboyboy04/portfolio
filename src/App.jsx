e223fb2b334e584fe8fb7932c6d4d854d2658538
biboyboy04 <55369988+biboyboy04@users.noreply.github.com>
2024-01-12 04:22:54 +0800

    Add Logo
    - Also add spin-slow animation for future ussage

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import ParticlesContainer from "./components/ParticlesContainer";
 import Home from "./components/Home";
+import Logo from "./components/Logo";
 export default function App() {
   return (
     <div className=" w-full xl:w-[1600px]">
       <ParticlesContainer />
+      <Logo />
       <Home />
     </div>
   );
---
+++ src/components/Logo.jsx
@@ -0,0 +1,8 @@
+const Logo = () => {
+  return (
+    <div className="hover: absolute left-0 top-0 z-10 w-[100px] transform transition-transform hover:scale-105">
+      <img src="/images/logo.png" />
+    </div>
+  );
+};
+export default Logo;
