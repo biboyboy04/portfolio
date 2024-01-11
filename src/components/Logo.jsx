7ff0ecb43e18374c649786dba5fa7093121eac1c
biboyboy04 <55369988+biboyboy04@users.noreply.github.com>
2024-01-12 04:39:27 +0800

    Change Logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,7 @@
 const Logo = () => {
   return (
-    <div className="hover: absolute left-0 top-0 z-10 w-[100px] transform transition-transform hover:scale-105">
-      <img src="/images/logo.png" />
+    <div className="absolute left-0 top-0 z-10 w-[100px] transform transition-transform hover:scale-105">
+      <img src="/images/logo1.png" />
     </div>
   );
 };
