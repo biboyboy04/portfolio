7c1392bb267919de5bf33d89e6085591efbf0343
biboyboy04 <55369988+biboyboy04@users.noreply.github.com>
2024-01-12 16:07:42 +0800

    Revert "Update App.jsx"
    This reverts commit 67bae5d95712cba0621a569a2e9e944a893b0722.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import ParticlesContainer from "./components/ParticlesContainer";
 import Home from "./components/Home";
 import Nav from "./components/Nav";
-import Logo from "./components/Logo";
 export default function App() {
   return (
     <div className=" w-full xl:w-[1600px]">
       <ParticlesContainer />
-      <Logo />
       <Nav />
       <Home />
     </div>
