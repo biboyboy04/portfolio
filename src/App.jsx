e33825b9f00593bf9852bcd2f02adb73eaec6b54
biboyboy04 <55369988+biboyboy04@users.noreply.github.com>
2024-01-12 16:12:12 +0800

    Use Logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import ParticlesContainer from "./components/ParticlesContainer";
 import Home from "./components/Home";
 import Nav from "./components/Nav";
+import Logo from "./components/Logo";
 export default function App() {
   return (
     <div className="w-full max-w-[100vw] xl:w-[1600px]">
       <ParticlesContainer />
+      <Logo />
       <Nav />
       <Home />
       <div className="page"></div>
