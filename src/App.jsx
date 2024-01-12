8f4b26d781a51e19251091c6ece482e406eb23dc
biboyboy04 <55369988+biboyboy04@users.noreply.github.com>
2024-01-12 15:32:00 +0800

    Add animations and Update Styles
    - Add framer-motion for animations
    
    - Add page class for that will be used for section containers.

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import ParticlesContainer from "./components/ParticlesContainer";
 import Home from "./components/Home";
 export default function App() {
   return (
-    <div className=" w-full xl:w-[1600px]">
+    <div className="w-full max-w-[100vw] xl:w-[1600px]">
       <ParticlesContainer />
       <Home />
+      <div className="page"></div>
     </div>
   );
 }
