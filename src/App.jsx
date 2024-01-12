bde74f32e80fefe564af9146d5a5073010489d67
biboyboy04 <55369988+biboyboy04@users.noreply.github.com>
2024-01-12 15:52:41 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import ParticlesContainer from "./components/ParticlesContainer";
 import Home from "./components/Home";
-import Logo from "./components/Logo";
-import Nav from "./components/Nav";
+
 export default function App() {
   return (
     <div className=" w-full xl:w-[1600px]">
       <ParticlesContainer />
-      <Logo />
       <Home />
     </div>
   );
